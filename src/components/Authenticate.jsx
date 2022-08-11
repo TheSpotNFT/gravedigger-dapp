563ed491e0457a86147581a5d2f28b95f9e27814
The Spot <96501055+TheSpotNFT@users.noreply.github.com>
2022-08-10 20:28:16 -0700

    Update Authenticate.jsx

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { useMoralis } from 'react-moralis'
-import goatdLogo from "../assets/goatd.png"
+import logo from "../assets/5.jpg"
 
 export default function Authenticate() {
     const  { authenticate } = useMoralis();
   return (
     <div className="bg-slate-900 flex w-full h-screen align-middle">
     <div className="m-auto">
-    <img src={goatdLogo} className="max-w-md" alt="Goatd Logo"></img>
+    <img src={logo} className="max-w-md" alt="Gravedigger Logo"></img>
     <div className="align-middle px-40 py-10">
     <button className="align-middle rounded-lg px-4 py-2 border-4 border-spot-yellow text-spot-yellow 
     hover:bg-spot-yellow hover:text-black duration-300 hover:border-white font-mono text-l" onClick={() => authenticate({ signingMessage: "Sign in to The Spot!" })}><b>Authenticate</b></button>
