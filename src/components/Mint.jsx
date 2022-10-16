0f3d4ef7eed0711d2af53f1a025c0c6f037e9998
The Spot <96501055+TheSpotNFT@users.noreply.github.com>
2022-10-16 09:36:33 -0700

    Add files via upload

--- src/components/Mint.jsx
+++ src/components/Mint.jsx
@@ -4,28 +4,47 @@ import { useWeb3ExecuteFunction, useMoralisCloudFunction } from "react-moralis";
 import spotNFTAbi from '../contracts/spotNFTAbi.json';
 import Moralis from 'moralis';
 import unnamedData from '../metadata';
+import unnamedAbi from '../contracts/spotNFTAbi.json';
+import nfTombstoneABI from '../contracts/nfTombstoneABI.json';
 
 
 
 function Mint(props) {
   const [isLoading, setIsLoading] = useState(false)
-  const spotTraitsContract = "0x9521807adf320d1cdf87afdf875bf438d1d92d87";
+  const nfTombstonesContract = "0x9521807adf320d1cdf87afdf875bf438d1d92d87";
   const spotNFTContract = '0x9455aa2aF62B529E49fBFE9D10d67990C0140AFC';
-  const background = props.chosenTrait.background;
-  const lines = props.chosenTrait.text;
-
+  const chosenNfTombstone = useState();
+  const chosenText = useState();
+  const contractProcessor = useWeb3ExecuteFunction();
+  const [isApproved, setIsApproved] = useState();
 
   let userAddress = props.userAddress
 
   const { data: mintData, error: mintError, fetch: mintFetch, isFetching: mintFetching, isLoading: mintLoading } = useWeb3ExecuteFunction();
 
-  function comingsoon() {
-    alert("Claim your plot soon...");
-  }
-
   function getImage() {
     return props.saveImage()
   }
+
+  async function isApprovedForAll() {
+    const approvedForAll = {
+      chain: "avalanche",
+      address: "0x6BDAd2A83a8e70F459786a96a0a9159574685c0e",
+      function_name: "isApprovedForAll",
+      abi: unnamedAbi,
+      params: {
+        owner: userAddress,
+        operator: "0xB043aaEb4337EA4BbB20C2ec5D846b00a0825ba5"
+      },
+    };
+    const areYouApproved = await Moralis.Web3API.native.runContractFunction(
+      approvedForAll
+    );
+    setIsApproved(areYouApproved);
+  }
+  useEffect(() => {
+    isApprovedForAll();
+  }, []);
 
   function checkTraits() {
     let isSafeBG = props.solidBG.some(ai => props.chosenTrait.BackgroundID === ai)
@@ -35,72 +54,95 @@ function Mint(props) {
     } else return false;
   }
 
-  async function mintMyNFT() {
-   /* if (!checkTraits()) {
-      alert("Some of the selected traits are not in your wallet. Ensure all trait-titles are yellow. Click 'My Owned Traits' again to refresh wallet traits.")
+  async function engraveTombstone() {
+
+    setIsLoading(true)
+    const base64 = await getImage()
+    const imageData = new Moralis.File("img.png", { base64: base64 });
+    await imageData.saveIPFS();
+    const imgURL = await imageData.ipfs();
+    console.log(imgURL)
+
+    const metadata = {
+      "name": "NFTombstone",
+      "description": "Engraved NFTombstone",
+      "image": imgURL,
+      "attributes": [
+        {
+          "trait_type": "Background:",
+          "value": props.unnamedEyes
+        },
+        {
+          "trait_type": "Behind",
+          "value": props.unnamedMouth
+        },
+        {
+          "trait_type": "Flair",
+          "value": props.unnamedHat
+        },
+        {
+          "trait_type": "Ground",
+          "value": props.unnamedSkin
+        },
+        {
+          "trait_type": "Tombstone",
+          "value": props.unnamedNose
+        },
+        {
+          "trait_type": "Top",
+          "value": props.unnamedSpecial
+        },
+        {
+          "trait_type": "Name",
+          "value": props.unnamedLines
+        },
+        {
+          "trait_type": "Epitaph",
+          "value": props.unnamedBrand
+        },
+
+      ],
     }
+    console.log(metadata)
 
-    else {*/
-      setIsLoading(true)
-      const base64 = await getImage()
-      const imageData = new Moralis.File("img.png", { base64: base64 });
-      await imageData.saveIPFS();
-      const imgURL = await imageData.ipfs();
 
-      const metadata = {
-        "name": "Tombstones",
-        "description": "Your TombStone",
-        "image": imgURL,
-        "attributes": [
-          {
-            "trait_type": "Tombstone",
-            "value": background
-          },
-          {
-            "trait_type": "Name",
-            "value": "name"
-          }
-        ],
-      }
 
-      console.log(metadata);
-      
-      /*const tokenMetadataUrlResult = await Moralis.Cloud.run("handlemint", {
-        metadata
-      });
-      const mintResult = await mintFetch({
-        params: {
-          abi: spotNFTAbi,
-          contractAddress: spotNFTContract,
-          functionName: "mint",
-          params: {
-            bg: props.chosenTrait.BackgroundID,
-            body: props.chosenTrait.BodyID,
-            head: props.chosenTrait.HeadID,
-            eyes: props.chosenTrait.EyesID,
-            mouth: props.chosenTrait.MouthID,
-            headwear: props.chosenTrait.HeadwearID,
-            uri: tokenMetadataUrlResult,
-          },
-          msgValue: Moralis.Units.ETH(0.3),
-        },
-        onError: (err) => {
-          setIsLoading(false)
-          alert(JSON.stringify(err.data.message));
+    const metaDataFile = new Moralis.File("file.json", { base64: btoa(JSON.stringify(metadata)) });
+    await metaDataFile.saveIPFS();
+    const metaDataUrl = await metaDataFile.ipfs();
+    console.log(metaDataUrl)
 
-        },
-        onSuccess: (tx) => {
-          tx.wait(2).then(alert("Minted successfully! View your NFT on Kalao or Campfire!"))
-            .then(setIsLoading(false))
-            .then(console.log(tx))
-        }
-      });
+    await Moralis.enableWeb3();
+    const sendOptions = {
+      contractAddress: "0xB043aaEb4337EA4BbB20C2ec5D846b00a0825ba5", //nfTombstone mainnet
+      functionName: "engraveTombstone",
+      abi: nfTombstoneABI,
+      msgValue: Moralis.Units.ETH(0.2),
+      params: {
+        _tokenID: props.id,
+        newTokenUri: metaDataUrl
+      },
+    };
+    const transaction = await contractProcessor.fetch({
+      params: sendOptions,
+      onError: (err) => {
+        setIsLoading(false);
+        alert(JSON.stringify(err.data.message));
+      },
+      onSuccess: (tx) => {
+        tx.wait(5)
+          .then(alert("Minted Successfully! View your NFT on Campfire, Kalao or Joepegs!"))
+          .then(setIsLoading(false))
+          .then(console.log(tx));
+      },
+    });
 
-    //}*/
   }
+
+
   if (isLoading) {
     return (
-      <div><button className="inline-flex m-2 rounded-lg px-4 py-2 border-2 border-spot-yellow text-spot-yellow
+      <div><button className="inline-flex m-1 rounded-lg px-4 py-2 border-2 border-spot-yellow text-spot-yellow
      duration-300 font-mono font-bold text-base" disabled>
         <svg className="inline animate-ping h-5 w-5 mr-3" viewBox="0 0 35 35">
           <circle className="path" cx="12" cy="15" r="10" fill="yellow" stroke="yellow" strokeWidth="2"></circle>
@@ -111,10 +153,20 @@ function Mint(props) {
     )
   } else
     return (
-      <div>
-        <button className="m-2 rounded-lg px-4 py-2 border-2 border-gray-200 text-gray-200
-     hover:bg-gray-200 hover:text-gray-900 duration-300 font-mono font-bold text-base" onClick={mintMyNFT} disabled={props.traitsAvailability === '1'}>Engrave</button>
+
+      <div className="flex w-full">
+        <div className="w-full pr-5 pl-1">
+          <button className="m-1 w-full rounded-lg px-1 py-1 border-2 border-gray-200 text-gray-200
+     hover:bg-gray-200 hover:text-gray-900 duration-300 font-mono font-bold text-base" onClick={engraveTombstone}>Engrave</button>
+
+
+        </div>
+
+
+
+
       </div>
+
     )
 }
 
