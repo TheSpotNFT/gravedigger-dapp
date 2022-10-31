24098621328a39586073e8cbb6644d8505685119
thejunglemarket <96501055+TheSpotNFT@users.noreply.github.com>
2022-10-30 21:11:25 -0700

    goatd routing

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -117,7 +117,7 @@ const Main = ({
               <button
                 className="align-middle rounded-lg px-4 py-2 border-4 border-spot-yellow text-spot-yellow 
       hover:bg-spot-yellow hover:text-black duration-300 hover:border-white font-mono text-l"
-                onClick={onClickUrl("https://thespot.wtf")}
+                onClick={onClickUrl("/goatd")}
               >
                 Enter the Transmorphisizer
               </button>
