d26529e26d909cf200b2ae5b6081f93206401f66
thejunglemarket <96501055+TheSpotNFT@users.noreply.github.com>
2023-02-23 15:15:39 -0800

    add .fire lookup

---
+++ src/components/Contracts/FireContract.js
@@ -0,0 +1,22 @@
+export const FIRE_ADDRESS = "0x44DD88c210C2052171165573368e13ecde5D9AE7";
+export const FIRE_ABI = [
+    {
+        "inputs": [
+          {
+            "internalType": "string",
+            "name": "username",
+            "type": "string"
+          }
+        ],
+        "name": "addressFor",
+        "outputs": [
+          {
+            "internalType": "address",
+            "name": "",
+            "type": "address"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      }
+]
--- src/components/vibesMints.jsx
+++ src/components/vibesMints.jsx
@@ -3,6 +3,10 @@ import {
   VIBES_ADDRESS,
   VIBES_ABI,
 } from "./Contracts/VibesContract";
+import {
+  FIRE_ADDRESS,
+  FIRE_ABI,
+} from "./Contracts/FireContract";
 import { Contract, ethers } from "ethers";
 
 export default function VibesMint(props) {
@@ -23,6 +27,42 @@ export default function VibesMint(props) {
     alert("vibes coming soon...");
   }
 
+  const [addressLookup, setAddressLookup] = useState();
+
+  async function getUsername() {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        if (FIRE_ABI && FIRE_ADDRESS && signer) {
+          const contract = new Contract(FIRE_ADDRESS, FIRE_ABI, signer);
+          let options = {
+
+          };
+          let result;
+          result = await contract.addressFor(textinput);
+          if (textinput.startsWith("0x")) {
+            setAddressLookup(textinput)
+          }
+          else {
+            setAddressLookup(result);
+          }
+
+
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+
+    }
+  }
+  useEffect(() => {
+    getUsername();
+  }, [textinput])
+  console.log(addressLookup);
+
   async function gud() {
     props.setTxProcessing(true);
     try {
@@ -42,7 +82,7 @@ export default function VibesMint(props) {
             value: ethers.utils.parseEther(`${numberinput * 0.2}`),
           };
 
-          let tx = await contract.mint(textinput, 1, numberinput);
+          let tx = await contract.mint(addressLookup, 1, numberinput);
           console.log(tx.hash);
           props.setTxProcessing(false);
           alert(
@@ -79,7 +119,7 @@ export default function VibesMint(props) {
             value: ethers.utils.parseEther(`${numberinput * 0.2}`),
           };
 
-          let tx = await contract.mint(textinput, 2, numberinput);
+          let tx = await contract.mint(addressLookup, 2, numberinput);
           console.log(tx.hash);
           props.setTxProcessing(false);
           alert(
@@ -133,7 +173,7 @@ export default function VibesMint(props) {
               <input
                 type="text"
                 className="border-2 h-3/4 border-slate-600 bg-slate-400 text-left font-mono placeholder-slate-600 pl-2 pr-4 w-52"
-                placeholder="receiving address"
+                placeholder=".fire or addr"
                 value={textinput}
                 onChange={textinputUser.bind(this)}
               />{" "}
@@ -166,8 +206,9 @@ hover:bg-red-500 hover:text-gray-900 duration-300 font-mono font-bold text-sm"
               Send badVibes
             </button>
           </div>
-        </div>
 
+        </div>
+        <div className="text-white font-mono pt-2">Address: {addressLookup !== "0x0000000000000000000000000000000000000000" ? addressLookup : ""}</div>
 
 
       </div>
