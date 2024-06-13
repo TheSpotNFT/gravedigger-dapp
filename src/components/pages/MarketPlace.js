5b33ab286657062ea7b898e240b65d284fc0a4b8
thejunglemarket <96501055+TheSpotNFT@users.noreply.github.com>
2024-06-12 17:11:48 -0700

    add transactions

--- src/components/pages/MarketPlace.js
+++ src/components/pages/MarketPlace.js
@@ -22,6 +22,7 @@ const MarketPlace = () => {
     const [isApproved, setIsApproved] = useState(false);
     const [tokenBalance, setTokenBalance] = useState(0);
     const [view, setView] = useState('list');
+    const [transfers, setTransfers] = useState([]);
     const navigate = useNavigate();
     const {
         account,
@@ -39,8 +40,8 @@ const MarketPlace = () => {
     const tokenIdToName = tokens.reduce((acc, token) => {
         acc[token.tokenId] = token.metadata.name;
         return acc;
-    }, {});    
-    
+    }, {});
+
     const fetchUserOrders = async () => {
         try {
             const { ethereum } = window;
@@ -48,7 +49,7 @@ const MarketPlace = () => {
                 const provider = new ethers.providers.Web3Provider(ethereum);
                 const signer = provider.getSigner();
                 const marketContract = new Contract(MARKET_ADDRESS, MARKET_ABI, signer);
-    
+
                 // Fetch all sell orders
                 const allSellOrders = [];
                 for (let token of tokens) {
@@ -59,7 +60,7 @@ const MarketPlace = () => {
                     allSellOrders.push(...userSellOrdersForToken);
                 }
                 setUserSellOrders(allSellOrders);
-    
+
                 // Fetch all buy orders
                 const allBuyOrders = [];
                 for (let token of tokens) {
@@ -75,69 +76,74 @@ const MarketPlace = () => {
             console.error("Error fetching user orders:", error);
         }
     };
-    
-    
+
+    const getTimeAgo = (timestamp) => {
+        const seconds = Math.floor((new Date() - new Date(timestamp * 1000)) / 1000);
+
+        let interval = seconds / 3600;
+        if (interval > 1) return Math.floor(interval) + " hours ago";
+
+        interval = seconds / 60;
+        if (interval > 1) return Math.floor(interval) + " minutes ago";
+
+        return Math.floor(seconds) + " seconds ago";
+    };
+
     useEffect(() => {
         if (tokens.length > 0 && account) {
             fetchUserOrders();
         }
     }, [tokens, account, showUserOrders]);
-    
 
- // Function to format very small values using scientific notation
-const formatSmallValue = (value) => {
-    if (value < 0.0001 && value !== 0) {
-      return value.toExponential(2); // 2 decimal places in exponent
-    }
-    return value.toFixed(8); // Default to 8 decimal places for small values
-  };
-  
-  // Function to add thousands separators
-  const formatLargeNumber = (number) => {
-    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  };
-  
-  // Function to format cryptocurrency prices
-  const formatPriceWithNotation = (price) => {
-    const parts = price.split('.');
-    if (parts.length === 1) return formatLargeNumber(price);
-  
-    const integerPart = formatLargeNumber(parts[0]);
-    let decimalPart = parts[1];
-  
-    if (parseFloat(`0.${decimalPart}`) < 0.0001 && parseFloat(`0.${decimalPart}`) !== 0) {
-      return formatSmallValue(parseFloat(price));
-    }
-  
-    // Trim trailing zeros from the decimal part
-    decimalPart = decimalPart.replace(/0+$/, '');
-  
-    return `${integerPart}.${decimalPart}`;
-  };
-  
-  const formatEtherWithNotation = (value) => {
-    const etherValue = parseFloat(ethers.utils.formatEther(value));
-    return formatPriceWithNotation(etherValue.toString());
-  };
+    const formatSmallValue = (value) => {
+        if (value < 0.0001 && value !== 0) {
+            return value.toExponential(2); // 2 decimal places in exponent
+        }
+        return value.toFixed(8); // Default to 8 decimal places for small values
+    };
 
-    // Function to add spaces every three digits
-const formatNumber = (number) => {
-    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-  };
-  
+    const formatLargeNumber = (number) => {
+        return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    };
+
+    const formatPriceWithNotation = (price) => {
+        const parts = price.split('.');
+        if (parts.length === 1) return formatLargeNumber(price);
+
+        const integerPart = formatLargeNumber(parts[0]);
+        let decimalPart = parts[1];
+
+        if (parseFloat(`0.${decimalPart}`) < 0.0001 && parseFloat(`0.${decimalPart}`) !== 0) {
+            return formatSmallValue(parseFloat(price));
+        }
+
+        // Trim trailing zeros from the decimal part
+        decimalPart = decimalPart.replace(/0+$/, '');
+
+        return `${integerPart}.${decimalPart}`;
+    };
+
+    const formatEtherWithNotation = (value) => {
+        const etherValue = parseFloat(ethers.utils.formatEther(value));
+        return formatPriceWithNotation(etherValue.toString());
+    };
+
+    const formatNumber = (number) => {
+        return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+    };
 
     const fetchAllItems = async () => {
         setLoading(true);
         let allFetchedTokens = [];
         let currentPageToken = null;
         const chainId = 43113;
-    
+
         try {
             while (true) {
                 const pageTokenParam = currentPageToken ? `&pageToken=${currentPageToken}` : "";
                 const url = `https://glacier-api.avax.network/v1/chains/${chainId}/nfts/collections/${SATS_ADDRESS}/tokens?pageSize=100${pageTokenParam}`;
                 const options = { method: "GET", headers: { accept: "application/json" } };
-    
+
                 const response = await axios.get(url, options);
                 const data = response.data;
                 if (Array.isArray(data.tokens)) {
@@ -151,10 +157,10 @@ const formatNumber = (number) => {
                     throw new Error(data.message || "Error fetching data");
                 }
             }
-    
+
             // Sort the tokens by latest minted (highest token ID number)
             allFetchedTokens.sort((a, b) => b.tokenId - a.tokenId);
-    
+
             // Fetch token details for each token
             const { ethereum } = window;
             if (ethereum) {
@@ -162,7 +168,7 @@ const formatNumber = (number) => {
                 const signer = provider.getSigner();
                 const satsContract = new Contract(SATS_ADDRESS, SATS_ABI, signer);
                 const marketContract = new Contract(MARKET_ADDRESS, MARKET_ABI, signer);
-    
+
                 const fetchedTokenDetails = await Promise.all(
                     allFetchedTokens.map(async (token) => {
                         const details = await satsContract.tokenDetails(token.tokenId);
@@ -180,12 +186,12 @@ const formatNumber = (number) => {
                         };
                     })
                 );
-    
+
                 const tokenDetailsMap = {};
                 fetchedTokenDetails.forEach(details => {
                     tokenDetailsMap[details.tokenId] = details;
                 });
-    
+
                 setTokens(allFetchedTokens);
                 setTokenDetails(tokenDetailsMap);
             }
@@ -195,7 +201,6 @@ const formatNumber = (number) => {
             setLoading(false);
         }
     };
-    
 
     const fetchOrders = async (tokenId) => {
         try {
@@ -338,26 +343,6 @@ const formatNumber = (number) => {
         }
     };
 
-    const buyToken = async (order) => {
-        try {
-            const { ethereum } = window;
-            if (ethereum) {
-                const provider = new ethers.providers.Web3Provider(ethereum);
-                const signer = provider.getSigner();
-                const marketContract = new Contract(MARKET_ADDRESS, MARKET_ABI, signer);
-                const totalCost = ethers.utils.parseUnits(order.price.toString(), 'wei').mul(order.amount);
-                const tx = await marketContract.buyToken(order.tokenId, order.price, order.amount, { value: totalCost.toString() });
-                await tx.wait(); // Wait for the transaction to be confirmed
-                console.log(tx.hash);
-                // Re-fetch orders after buying a token
-                fetchOrders(order.tokenId);
-            }
-        } catch (error) {
-            console.error("Error buying token:", error);
-            alert("An error occurred while buying the token.");
-        }
-    };
-
     const buyUpToLimit = async (tokenId, avaxLimit) => {
         try {
             const { ethereum } = window;
@@ -365,8 +350,8 @@ const formatNumber = (number) => {
                 const provider = new ethers.providers.Web3Provider(ethereum);
                 const signer = provider.getSigner();
                 const marketContract = new Contract(MARKET_ADDRESS, MARKET_ABI, signer);
-                const tx = await marketContract.buyUpToLimit(tokenId, ethers.utils.parseEther(avaxLimit), { 
-                    value: ethers.utils.parseEther(avaxLimit), 
+                const tx = await marketContract.buyUpToLimit(tokenId, ethers.utils.parseEther(avaxLimit), {
+                    value: ethers.utils.parseEther(avaxLimit),
                     gasLimit: 3000000 // Increase the gas limit
                 });
                 await tx.wait(); // Wait for the transaction to be confirmed
@@ -385,6 +370,26 @@ const formatNumber = (number) => {
         await fetchOrders(token.tokenId);
         await fetchBalance(token.tokenId);
         checkApproval();
+        fetchRecentTransfers(token.tokenId);
+    };
+
+    const fetchRecentTransfers = async (tokenId) => {
+        const chainId = 43113;
+        const url = `https://glacier-api.avax.network/v1/chains/${chainId}/addresses/${SATS_ADDRESS}/transactions`;
+        const options = { method: "GET", headers: { accept: "application/json" } };
+
+        try {
+            const response = await axios.get(url, options);
+            const data = response.data;
+            if (Array.isArray(data.transactions)) {
+                setTransfers(data.transactions);
+                console.log(transfers);
+            } else {
+                console.error("No transactions found");
+            }
+        } catch (err) {
+            console.error("Fetch error:", err);
+        }
     };
 
     useEffect(() => {
@@ -400,32 +405,29 @@ const formatNumber = (number) => {
 
     return (
         <div className="relative min-h-screen font-mono text-white w-full md:w-3/5 sm:pt-24">
-
-<div className="flex justify-between mb-4 space-x-2">
-    <button
-        onClick={() => navigate('/sats')}
-        className="flex-1 px-4 py-2 rounded bg-blue-500 hover:bg-blue-700 text-white font-bold"
-    >
-        Create SATs
-    </button>
-    <button
-        onClick={() => navigate('/satsgallery')}
-        className="flex-1 px-4 py-2 rounded bg-blue-500 hover:bg-blue-700 text-white font-bold"
-    >
-        Mint SATs
-    </button>
-    <button
-        onClick={() => navigate('/market')}
-        disabled={true}
-    className={`flex-1 px-4 py-2 rounded font-bold text-white ${
-        true ? 'bg-blue-700' : 'bg-blue-500 hover:bg-blue-700'
-    }`}
-    >
-        SATs Marketplace
-    </button>
-</div>
-
-
+            <div className="flex justify-between mb-4 space-x-2">
+                <button
+                    onClick={() => navigate('/sats')}
+                    className="flex-1 px-4 py-2 rounded bg-blue-500 hover:bg-blue-700 text-white font-bold"
+                >
+                    Create SATs
+                </button>
+                <button
+                    onClick={() => navigate('/satsgallery')}
+                    className="flex-1 px-4 py-2 rounded bg-blue-500 hover:bg-blue-700 text-white font-bold"
+                >
+                    Mint SATs
+                </button>
+                <button
+                    onClick={() => navigate('/market')}
+                    disabled={true}
+                    className={`flex-1 px-4 py-2 rounded font-bold text-white ${
+                        true ? 'bg-blue-700' : 'bg-blue-500 hover:bg-blue-700'
+                    }`}
+                >
+                    SATs Marketplace
+                </button>
+            </div>
             <h1 className="text-3xl font-bold mb-4 pt-16">SATs Marketplace</h1>
             <div className="flex justify-end mb-4">
                 <button
@@ -463,7 +465,7 @@ const formatNumber = (number) => {
                     {tokens.map((token) => {
                         const details = tokenDetails[token.tokenId];
                         if (!details) return null;
-    
+
                         return (
                             <div key={token.tokenId} className="bg-gray-800 p-4 rounded shadow cursor-pointer" onClick={() => selectToken(token)}>
                                 <img 
@@ -520,7 +522,7 @@ const formatNumber = (number) => {
                     {tokens.map((token) => {
                         const details = tokenDetails[token.tokenId];
                         if (!details) return null;
-    
+
                         return (
                             <div key={token.tokenId} className="bg-gray-800 p-4 shadow flex items-center cursor-pointer" onClick={() => selectToken(token)}>
                                 <img 
@@ -558,7 +560,7 @@ const formatNumber = (number) => {
                     })}
                 </div>
             )}
-    
+
             {selectedToken && (
                 <div className="fixed inset-0 flex items-start justify-center pt-16 bg-black bg-opacity-75 overflow-y-auto">
                     <div className="bg-gray-900 p-4 rounded shadow-lg max-w-6xl w-full relative">
@@ -568,13 +570,35 @@ const formatNumber = (number) => {
                         >
                             Close
                         </button>
-                        <h2 className="text-4xl font-bold mb-2 text-spot-yellow">{selectedToken.metadata.name}</h2>
-                        <img 
-                            src={`https://gateway.ipfs.io/ipfs/${selectedToken.metadata.imageUri.split("ipfs://")[1]}`} 
-                            alt={selectedToken.metadata.name} 
-                            className="w-1/2 mx-auto h-auto mb-2" 
-                        />
-                        
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                            <div>
+                                <h2 className="text-4xl font-bold mb-2 text-spot-yellow">{selectedToken.metadata.name}</h2>
+                                <img 
+                                    src={`https://gateway.ipfs.io/ipfs/${selectedToken.metadata.imageUri.split("ipfs://")[1]}`} 
+                                    alt={selectedToken.metadata.name} 
+                                    className="w-full h-auto mb-2 rounded" 
+                                />
+                            </div>
+                            <div>
+                                <h3 className="text-xl font-bold mb-4">Recent Transfers</h3>
+                                <div className="grid grid-cols-6 gap-4 pb-4 border-b-2 border-gray-700">
+                <div className="text-gray-400 col-span-1">Seller</div>
+                <div className="text-gray-400 col-span-1">Buyer</div>
+                <div className="text-gray-400 col-span-2">Value</div>
+                <div className="text-gray-400 col-span-2">Time Ago</div>
+            </div>
+                              
+                                {transfers.map((tx, index) => (
+                <div key={index} className="grid grid-cols-6 gap-4 py-2 border-b-2 border-gray-700">
+                    <div className="col-span-1 text-red-700">{tx.nativeTransaction.from.address.slice(-4)}</div>
+                    <div className="col-span-1 text-green-700">{tx.nativeTransaction.to.address.slice(-4)}</div>
+                    <div className="col-span-2">{formatEtherWithNotation(tx.nativeTransaction.value.toString())} AVAX</div>
+                    <div className="col-span-2">{getTimeAgo(tx.nativeTransaction.blockTimestamp)}</div>
+                </div>
+            ))}
+                                
+                            </div>
+                        </div>
                         <p className="text-center pb-4 pt-4">Your Token Balance: {tokenBalance}</p>
                         <p className="pb-4 text-spot-yellow font-bold text-lg">Market Cap: {tokenDetails[selectedToken.tokenId]?.marketCap} AVAX</p>
                         {!isApproved && (
@@ -716,7 +740,7 @@ const formatNumber = (number) => {
                     </div>
                 </div>
             )}
-    
+
             {showUserOrders && (
                 <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75">
                     <div className="bg-gray-900 p-4 rounded shadow-lg max-w-6xl w-full relative">
@@ -792,14 +816,13 @@ const formatNumber = (number) => {
                     </div>
                 </div>
             )}
-                 <footer className=" text-white text-center py-4 pt-24">
-    <p>
-        As a platform, we do not condone or support any tokens created or traded on this platform. All users are responsible for their own actions and decisions regarding token transactions. Use at your own risk.
-    </p>
-</footer>
+            <footer className=" text-white text-center py-4 pt-24">
+                <p>
+                    As a platform, we do not condone or support any tokens created or traded on this platform. All users are responsible for their own actions and decisions regarding token transactions. Use at your own risk.
+                </p>
+            </footer>
         </div>
     );
-    
 };
 
 export default MarketPlace;
